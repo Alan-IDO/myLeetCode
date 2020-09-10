db2d23d92d3d1b01baf2613611795dce4bc7db2e
Alan-IDO <43458018+Alan-IDO@users.noreply.github.com>
2020-09-11 00:50:30 +0800

    Remove Element
    给定一个数组和一个数字，在该数组中删除该数字。

---
+++ leetCode_27.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let Length = nums.length;
+    for(let i = 0; i<Length; i++)
+    {
+        if(nums[i] == val)
+        {
+            nums.splice(i,1);
+            i--;
+        }
+    }
+};
